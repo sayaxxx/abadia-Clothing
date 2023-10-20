c2dd4e62905df4a67d6974b4edfcf8f94e94fb96
KonnyAlucema <lisethtorres969@gmail.com>
2023-10-20 18:42:22 -0500

    feat: :sparkles: Configuracion del ApiContext

--- Persistencia/ApiContext.cs
+++ Persistencia/ApiContext.cs
@@ -11,6 +11,30 @@ public class ApiContext : DbContext
     public DbSet<Rol> Roles { get; set; }
     public DbSet<RolUsuario> RolUsuarios { get; set; }
     public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Cargo> Cargos { get; set; }
+    public DbSet<Color> Colores { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<DetalleOrden> DetalleOrdenes { get; set; }
+    public DbSet<DetalleVenta> DetalleVentas { get; set; }
+    public DbSet<Empleado> Empleados { get; set; }
+    public DbSet<Empresa> Empresas { get; set; }
+    public DbSet<Estado> Estados { get; set; }
+    public DbSet<FormaPago> FormaPagos { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+    public DbSet<Insumo> Insumos { get; set; }
+    public DbSet<InsumoPrenda> InsumoPrendas { get; set; }
+    public DbSet<InsumoProveedor> InsumoProveedores { get; set; }
+    public DbSet<Inventario> Inventarios { get; set; }
+    public DbSet<InventarioTalla> InventarioTallas { get; set; }
+    public DbSet<Municipio> Municipios { get; set; }
+    public DbSet<Orden> Ordenes { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Prenda> Prendas { get; set; }
+    public DbSet<Proveedor> Proveedores { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
