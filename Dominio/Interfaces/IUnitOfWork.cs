4c3b8651052b49acf9e947d0ac4c278461c2d17a
KonnyAlucema <lisethtorres969@gmail.com>
2023-10-20 19:25:04 -0500

    feat: :sparkles: Unidad De Trabajo de Interfaces

--- Dominio/Interfaces/IUnitOfWork.cs
+++ Dominio/Interfaces/IUnitOfWork.cs
@@ -4,4 +4,28 @@ public interface IUnitOfWork
     IRol Roles { get; }
     IUsuario Usuarios { get; }
     Task<int> SaveAsync();
+
+    ICargo Cargos { get; }
+    ICliente Clientes { get; }
+    IColor Colores { get; }
+    IDepartamento Departamentos { get; }
+    IDetalleOrden DetalleOrdenes { get; }
+    IDetalleVenta DetalleVentas { get; }
+    IEmpleado Empleados { get; }
+    IEmpresa Empresas { get; }
+    IEstado Estados { get; }
+    IFormaPago FormaPagos { get; }
+    IGenero Generos { get; }
+    IInsumo Insumos { get; }
+    IInventario Inventarios { get; }
+    IMunicipio Municipios { get;}
+    IOrden Ordenes { get; }
+    IPais Paises { get; }
+    IPrenda Prendas { get; }
+    IProveedor Proveedores { get; }
+    ITalla Tallas { get; }
+    ITipoEstado TipoEstados { get; }
+    ITipoPersona TipoPersonas { get; }
+    IVenta Ventas { get; }
+
 }
